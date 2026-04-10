61576f53e36507622ccd6f2b746187a00f9e336f
shivani552 <tammisettishivani@gmail.com>
2026-04-10 18:57:11 +0530

    [UI] added routing and URL

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
+import QuestionPage from './pages/QuestionPage';
 
 function App() {
   return (
-    <div>
-      <Home />
-
-      {/* TODO: Replace this placeholder with Routes */}
-    </div>
+    <Router>
+      <Routes>
+        {/* Default route goes to Home */}
+        <Route path="/" element={<Home />} />
+        
+        {/* The tasks route will handle the status filtering in the URL */}
+        <Route path="/tasks" element={<QuestionPage />} />
+        
+        {/* Fallback to Home */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 }
 
